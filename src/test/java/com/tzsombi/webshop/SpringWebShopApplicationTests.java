052bfb7e6c6a2dda955de5ae9c2f36af71c1cb99
tzsombi01 <toreky.zsombor@gmail.com>
2023-01-20 16:58:54 +0100

    Initial Commit

---
+++ src/test/java/com/tzsombi/webshop/SpringWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzsombi.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

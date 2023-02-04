9f1f3e487cf4e910ed02be921d9fc9d718ecbadb
tzsombi01 <toreky.zsombor@gmail.com>
2023-02-04 12:30:06 +0100

    Implemented Product Paging and Sorting

--- src/main/java/com/tzsombi/webshop/controllers/ProductController.java
+++ src/main/java/com/tzsombi/webshop/controllers/ProductController.java
@@ -3,11 +3,10 @@ package com.tzsombi.webshop.controllers;
 import com.tzsombi.webshop.models.Product;
 import com.tzsombi.webshop.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,8 +16,13 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
+    public ResponseEntity<Page<Product>> getAllProducts(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer numberOfProductsPerPage,
+            @RequestParam(defaultValue = "true") Boolean isOrderAsc,
+            @RequestParam(defaultValue = "name") String sortBy
+            ) {
+        Page<Product> products = productService.getAllProducts(page, numberOfProductsPerPage, sortBy, isOrderAsc);
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
--- src/main/java/com/tzsombi/webshop/services/ProductService.java
+++ src/main/java/com/tzsombi/webshop/services/ProductService.java
@@ -4,9 +4,10 @@ import com.tzsombi.webshop.models.Product;
 import com.tzsombi.webshop.repositories.ProductRepository;
 import com.tzsombi.webshop.utils.ProductFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +15,9 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public Page<Product> getAllProducts(Integer page, Integer numberOfProducts, String sortBy, Boolean isOrderAsc) {
+        return productRepository.findAll(
+                PageRequest.of(page, numberOfProducts, isOrderAsc ? Sort.Direction.ASC : Sort.Direction.DESC, sortBy));
     }
 
     public void addProduct(String rawProduct) {

adb51f74d3497a4cbe65311748f0c9f52f7a8fad
tzsombi01 <toreky.zsombor@gmail.com>
2023-06-01 21:42:08 +0200

    fixed(products-credit-card-section): If no products or credit-cards are available, a text will pop up

--- src/frontend/spring-webshop-fe/src/app/profile/components/profile-body/profile-body.component.ts
+++ src/frontend/spring-webshop-fe/src/app/profile/components/profile-body/profile-body.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CreditCardResponse } from 'src/app/interfaces/creditCardResponse';
+import { Product } from 'src/app/interfaces/product';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -10,9 +12,25 @@ export class ProfileBodyComponent {
 
   protected displayCategory: string = "sellingProducts";
 
-  constructor(protected userService: UserService) { }
+  constructor(private userService: UserService) { }
 
   public setDisplayCategory(displayCategory: string): void {
     this.displayCategory = displayCategory;
+  }
+
+  public getCards(): CreditCardResponse[] {
+    return this.userService.user?.creditCards ?? [];
+  }
+
+  public getProducts(): Product[] {
+    return this.userService.user?.sellingProducts;
+  }
+
+  public productsAreEmpty(): boolean {
+    return (this.userService.user?.sellingProducts?.length ?? 0) === 0 ;
+  }
+
+  public creditCardsAreEmpty(): boolean {
+    return (this.userService.user?.creditCards?.length ?? 0) === 0 ;
   }
 }
